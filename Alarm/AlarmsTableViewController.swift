a5ef80d061a7a59447a908931e72248eab42924d
lpuentes19 <luey.puentes@gmail.com>
2017-08-15 14:47:24 -0600

    Created the custom tableview cell and embedded it in a stack view and constrained it

---
+++ Alarm/AlarmsTableViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AlarmsTableViewController.swift
+//  Alarm
+//
+//  Created by Luis Puentes on 8/15/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class AlarmsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "alarmCell", for: indexPath)
+
+
+        return cell
+    }
+    
+    // Delete Rows
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+}
