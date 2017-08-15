85600bd13e2d67be14a83a313ce2959d861bdb3f
lpuentes19 <luey.puentes@gmail.com>
2017-08-15 14:57:11 -0600

    Completed both views in the storyboard. Will connect IBOutlets and Actions

---
+++ Alarm/AlarmDetailTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  AlarmDetailTableViewController.swift
+//  Alarm
+//
+//  Created by Luis Puentes on 8/15/17.
+//  Copyright © 2017 LuisPuentes. All rights reserved.
+//
+
+import UIKit
+
+class AlarmDetailTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
